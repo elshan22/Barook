66ea6f08faf6459e0778e7c0c5500336c6452251
elshan22 <elshansaber@gmail.com>
2025-02-07 16:36:27 +0330

    add controller

--- bank/src/main/java/com/example/bank/controller/BankController.java
+++ bank/src/main/java/com/example/bank/controller/BankController.java
@@ -1,5 +1,35 @@
 package com.example.bank.controller;
 
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.bank.dto.TransactionDTO;
+import com.example.bank.dto.WalletDTO;
+import com.example.bank.service.TransactionService;
+import com.example.bank.service.WalletService;
+
+@RestController
+@RequestMapping("")
 public class BankController {
-    
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @Autowired
+    private WalletService walletService;
+
+    @GetMapping("/balance/{userId}")
+    public WalletDTO getBalance(@PathVariable Long userId) {
+        BigDecimal balance = walletService.getBalance(userId);
+        return new WalletDTO(balance);
+    }
+
+    @PostMapping("/add/{userId}")
+    public TransactionDTO addAmount(@PathVariable Long userId, @RequestParam double amount) {
+        BigDecimal bigAmount = BigDecimal.valueOf(amount);
+        Long reference_id = transactionService.addAmount(userId, bigAmount);
+        return new TransactionDTO(reference_id);
+    }
 }
