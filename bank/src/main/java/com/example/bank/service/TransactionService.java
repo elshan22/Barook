a33d982846b1283fc9cba5bad42c965ef349bff6
elshan22 <elshansaber@gmail.com>
2025-02-07 16:17:24 +0330

    add services

--- bank/src/main/java/com/example/bank/service/TransactionService.java
+++ bank/src/main/java/com/example/bank/service/TransactionService.java
@@ -1,5 +1,34 @@
 package com.example.bank.service;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.bank.model.Transaction;
+import com.example.bank.model.Wallet;
+import com.example.bank.repository.TransactionRepository;
+import com.example.bank.repository.WalletRepository;
+
+@Service
 public class TransactionService {
-    
+
+    @Autowired
+    private WalletRepository walletRepository;
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    public Long addAmount(Long userId, BigDecimal amount) {
+        Optional<Wallet> wallet = walletRepository.findByUserId(userId);
+        if (wallet.isPresent()) {
+            wallet.get().setBalance(wallet.get().getBalance().add(amount));
+            walletRepository.save(wallet.get());
+            Transaction transaction = new Transaction(userId, amount);
+            transactionRepository.save(transaction);
+            return transaction.getId();
+        }
+        return null;
+    }
 }
--- bank/src/main/java/com/example/bank/service/WalletService.java
+++ bank/src/main/java/com/example/bank/service/WalletService.java
@@ -1,5 +1,22 @@
 package com.example.bank.service;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.bank.model.Wallet;
+import com.example.bank.repository.WalletRepository;
+
+@Service
 public class WalletService {
-    
+
+    @Autowired
+    private WalletRepository walletRepository;
+
+    public BigDecimal getBalance(Long userId) {
+        Optional<Wallet> wallet = walletRepository.findByUserId(userId);
+        return wallet.isPresent()? wallet.get().getBalance() : null;
+    }
 }
