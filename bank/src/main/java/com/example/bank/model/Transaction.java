4c45733e47f54a292d2962289af31fe2e3eeb56f
elshan22 <elshansaber@gmail.com>
2025-02-07 15:47:31 +0330

    define models

--- bank/src/main/java/com/example/bank/model/Transaction.java
+++ bank/src/main/java/com/example/bank/model/Transaction.java
@@ -1,5 +1,35 @@
 package com.example.bank.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;;
+
+@Entity
+@Table(name = "transactions")
+@NoArgsConstructor
+@Getter
 public class Transaction {
-    
+    @Id
+    private Long id;
+
+    @Column(nullable=false)
+    private Long userId;
+
+    @Column(nullable=false)
+    private BigDecimal amount;
+
+    private LocalDateTime timestamp;
+
+    public Transaction(Long userId, BigDecimal amount) {
+        this.userId = userId;
+        this.amount = amount;
+        this.timestamp = LocalDateTime.now();
+    }
 }
--- bank/src/main/java/com/example/bank/model/Wallet.java
+++ bank/src/main/java/com/example/bank/model/Wallet.java
@@ -1,5 +1,38 @@
 package com.example.bank.model;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "wallets")
+@NoArgsConstructor
+@Getter
 public class Wallet {
+    @Id
+    private Long id;
     
+    @Column(nullable=false, unique=true)
+    private Long userId;
+    
+    @Column(nullable=false)
+    @Setter
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    public Wallet(Long userId) {
+        this.userId = userId;
+        this.balance = BigDecimal.ZERO;
+    }
+
+    public Wallet(Long userId, BigDecimal balance) {
+        this.userId = userId;
+        this.balance = balance;
+    }
 }
