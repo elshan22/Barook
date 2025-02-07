4d3cef091e10e8a4944e4f648ead94a1e94c7491
elshan22 <elshansaber@gmail.com>
2025-02-07 16:29:26 +0330

    add dto

--- bank/src/main/java/com/example/bank/dto/TransactionDTO.java
+++ bank/src/main/java/com/example/bank/dto/TransactionDTO.java
@@ -1,5 +1,25 @@
 package com.example.bank.dto;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class TransactionDTO {
-    
+
+    private Long reference_id;
+
+    public TransactionDTO(Long reference_id) {
+        this.reference_id = reference_id;
+    }
 }
--- bank/src/main/java/com/example/bank/dto/WalletDTO.java
+++ bank/src/main/java/com/example/bank/dto/WalletDTO.java
@@ -1,5 +1,19 @@
 package com.example.bank.dto;
 
+import java.math.BigDecimal;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class WalletDTO {
-    
+
+    private BigDecimal balance;
+
+    public WalletDTO(BigDecimal balance) {
+        this.balance = balance;
+    }
 }
